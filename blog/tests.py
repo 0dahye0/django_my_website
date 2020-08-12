d2d6633bb46f2f43cb18d1c609c84ea0f0643b90
0dahye0 <51871061+0dahye0@users.noreply.github.com>
2020-08-13 00:48:29 +0900

    Update tests.py

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+def create_comment(post, text='a comment', author=anonymous):
+  comment = Comment.objects.create(
+    post=post,
+    text=text,
+    author=anonymous,
+  )
+  
+  
+def create_post(title, content, author, category = None):
+  blog_post = Post.object.create(
+    title=title,
+    content=content,
+    created=timezone.now(),
+    author=author,
+    category=category,
+  )
+  
+  
+def test_comment(self):
+  comment = create_comment:
+    post= post_000
+    
